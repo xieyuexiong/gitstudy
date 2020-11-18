251e035d035b68b99e18665bfd47f2af69bb1140
xieyuexiong <1948689271@qq.com>
2020-11-18 17:57:36 +0800

    1111

---
+++ src/main/java/com/xyx/gitstuidy/GitstuidyApplication.java
@@ -0,0 +1,13 @@
+package com.xyx.gitstuidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstuidyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitstuidyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xyx/gitstuidy/GitstuidyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyx.gitstuidy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstuidyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
